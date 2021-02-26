0f8efcf74c65a1f03027409256db438e44795aa4
mattianeroni <54416801+mattianeroni@users.noreply.github.com>
2021-02-26 14:51:48 +0100

    Create main.cc

---
+++ main.cc
@@ -0,0 +1,36 @@
+// *******************************************************************
+// ----------------------------- TEST --------------------------------
+// *******************************************************************
+
+
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+
+
+void putter (chan<int>* c, int element){
+  c->put(element);
+}
+
+
+void getter (chan<int>* c) {
+  int a = c->get();
+  cout << "Retrieved " << a << "\n";
+}
+
+
+int main () {
+
+  chan<int> c;
+
+  thread t1 (putter, &c, 10);
+  thread t2 (putter, &c, 3);
+  thread t3 (getter, &c);
+
+  t1.join(); t2.join(); t3.join();
+
+  cout << "Program correctly concluded.\n";
+  return 0;
+}
